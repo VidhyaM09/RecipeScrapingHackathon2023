ad0273cc18a76389f99ca4f6fb7f0aefd3155607
VidhyaM09 <vidhyamanickaraj@gmail.com>
2023-12-13 22:28:01 -0600

    Recipe Scraping for Diabetes

--- Team02_NinjaScrapers_2023/src/test/java/com/recipesfordiabetescondition/GetSetRecipes.java
+++ Team02_NinjaScrapers_2023/src/test/java/com/recipesfordiabetescondition/GetSetRecipes.java
@@ -1,10 +1,32 @@
 package com.recipesfordiabetescondition;
 
+import org.openqa.selenium.WebElement;
+
 public class GetSetRecipes {
 
 	private String recipeName;
 
 	private String recipeId;
+	
+	private String recipeCategory;
+	
+	public String getRecipeCategory() {
+		return recipeCategory;
+	}
+
+	public void setRecipeCategory(String string) {
+		this.recipeCategory = string;
+	}
+
+	private WebElement insideRecipeDetails;
+
+	public WebElement getInsideRecipeDetails() {
+		return insideRecipeDetails;
+	}
+
+	public WebElement setInsideRecipeDetails(WebElement webElement) {
+		return this.insideRecipeDetails = webElement;
+	}
 
 	public String getRecipeId() {
 		return recipeId;
